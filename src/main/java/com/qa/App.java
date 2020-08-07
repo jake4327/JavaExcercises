5a5d9c231695aad5a620ec6054fe847c32da9009
Jake Stone <jakestone4327@gmail.com>
2020-08-07 15:24:55 +0100

    Trail 2 push

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Everyone!" );
+        System.out.println( "Hello Everyone trial 2!" );
     }
 }
