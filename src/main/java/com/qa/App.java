c419eb847adabf47e1d56f86f36f27400a7c8ccd
Jake Stone <jakestone4327@gmail.com>
2020-08-10 15:11:11 +0100

    hello world exercises

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -7,7 +7,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Everyone trial 2!" );
-        System.out.println(5+5);
         Fizzbuzz.check1();
+        Fizzbuzz.check1();
+        System.out.println("string 1" + "string 2");
+        System.out.println(5+5);
     }
+
 }
---
+++ src/main/java/com/qa/day2/helloWorld.java
@@ -0,0 +1,18 @@
+package com.qa.day2;
+
+public class helloWorld {
+    //System.out.println("Hello world")
+
+    public static void main(String[] args) {
+        printOne("hello world");
+        System.out.println(printTwo("Hello world"));
+    }
+
+    public static void printOne(String word){
+        System.out.println(word);
+    }
+
+    public static String printTwo(String word){
+        return word;
+    }
+}
