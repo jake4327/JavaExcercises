565b02f4c854fc876f1c83fdc8681b90670b30e5
Jake Stone <jakestone4327@gmail.com>
2020-08-10 13:17:08 +0100

    refactor shortcut shift F6

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -1,10 +1,13 @@
 package com.qa;
 
-public class App 
+import com.qa.day2.Fizzbuzz;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello Everyone trial 2!" );
         System.out.println(5+5);
+        Fizzbuzz.check1();
     }
 }
--- src/main/java/com/qa/day2/Fizzbuzz.java
+++ src/main/java/com/qa/day2/Fizzbuzz.java
@@ -1,7 +1,13 @@
 package com.qa.day2;
 
-public class Fizzbuzz{
+public class Fizzbuzz {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+
+    public static Boolean check1() {
+        return true;
+    }
+
 }
