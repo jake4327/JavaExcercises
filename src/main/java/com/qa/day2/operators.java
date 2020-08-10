c029578f526d745c99854af10708502a0e1ef245
Jake Stone <jakestone4327@gmail.com>
2020-08-10 15:20:14 +0100

    First calculator push

---
+++ src/main/java/com/qa/day2/operators.java
@@ -0,0 +1,22 @@
+package com.qa.day2;
+
+public class operators {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int addition(int numberOne, int numberTwo){
+        return numberOne + numberTwo;
+    }
+
+    public static int multiplication(int numberOne, int numberTwo){
+        return numberOne + numberTwo;
+    }
+
+    public static int division(int numberOne, int numberTwo){
+        return numberOne / numberTwo;
+    }
+
+
+}
